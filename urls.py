4ef4f99d2c635dc49140348884a2fcb636622936
alanam001 <alanamullins77@gmail.com>
2024-11-25 01:07:33 -0700

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+
+    path('', include('restaurant.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('api/', include('api.urls')),
+    path('api/token/login/', TokenObtainPairView.as_view(), name='token-login'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('api-token-auth/', obtain_auth_token)
+]
